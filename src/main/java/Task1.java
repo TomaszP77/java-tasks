f820c57400f2e43bb3d96f6fc6c695eaeb5ff88a
Tomasz <feat24@wp.pl>
2023-11-13 18:25:43 +0100

    Add: solution Task1

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,2 +1,17 @@
 public class Task1 {
+
+    public static void main(String[] args) {
+
+        int a = 50;
+        int b = 30;
+        int c = a + b;
+        int d = c * 30;
+        int e = d % 70;
+
+        if (e > 15) {
+            System.out.println("The remainder of the division is greater than 15");
+        } else {
+            System.out.println("The remainder of the division is less than 15");
+        }
+    }
 }
