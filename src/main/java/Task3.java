b2dd99b693ed1ee0bb8273613a9722892fee335a
Tomasz <feat24@wp.pl>
2023-11-15 21:20:52 +0100

    Add: solution Task3

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -1,2 +1,48 @@
 public class Task3 {
+
+    public static void main(String[] args) {
+
+        String month = "December";
+
+        switch (month) {
+            case "December":
+                System.out.println("Winter");
+                break;
+            case "January":
+                System.out.println("Winter");
+                break;
+            case "February":
+                System.out.println("Winter");
+                break;
+            case "March":
+                System.out.println("Spring");
+                break;
+            case "April":
+                System.out.println("Spring");
+                break;
+            case "May":
+                System.out.println("Spring");
+                break;
+            case "June":
+                System.out.println("Summer");
+                break;
+            case "July":
+                System.out.println("Summer");
+                break;
+            case "August":
+                System.out.println("Summer");
+                break;
+            case "September":
+                System.out.println("Autumn");
+                break;
+            case "October":
+                System.out.println("Autumn");
+                break;
+            case "November":
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("Please enter the correct month name");
+        }
+    }
 }
