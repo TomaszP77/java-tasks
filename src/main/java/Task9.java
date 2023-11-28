1c954c37e420ddaa4b1ccce83b23b983e7fc32d3
Tomasz <feat24@wp.pl>
2023-11-28 12:42:45 +0100

    Add solution Task9

--- src/main/java/Task9.java
+++ src/main/java/Task9.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Task9 {
+
+    public static void main(String[] args) {
+        String word1;
+        String word2;
+
+        System.out.println("Enter the first word");
+        word1 = getWord();
+
+        System.out.println("Enter the second word");
+        word2 = getWord();
+
+        System.out.println(word2 + " " + word1);
+    }
+
+    public static String getWord() {
+        return new Scanner(System.in).next();
+    }
 }
