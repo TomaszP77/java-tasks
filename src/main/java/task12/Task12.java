54a03d4ef560d5e7d821b3ddd24e5d98218d1766
Tomasz <feat24@wp.pl>
2023-12-11 09:58:00 +0100

    Add solution Task12

--- src/main/java/task12/Task12.java
+++ src/main/java/task12/Task12.java
@@ -3,4 +3,33 @@ package task12;
 import java.util.Scanner;
 
 public class Task12 {
+
+    public static void main(String[] args) {
+        int firstNumber;
+        int secondNumber;
+
+        System.out.println("enter the first number");
+        firstNumber = getInt();
+        System.out.println("enter the second number");
+        secondNumber = getInt();
+
+        BasicMathematicalOperations calculator = new BasicMathematicalOperations();
+
+
+        int addition = calculator.addition(firstNumber, secondNumber);
+        int subtraction = calculator.subtraction(firstNumber, secondNumber);
+        int multiplication = calculator.multiplication(firstNumber, secondNumber);
+        int division = calculator.division(firstNumber, secondNumber);
+        int modulo = calculator.modulo(firstNumber, secondNumber);
+
+        System.out.println("Addition - " + addition);
+        System.out.println("Subtraction - " + subtraction);
+        System.out.println("Multiplication - " + multiplication);
+        System.out.println("Division - " + division);
+        System.out.println("Modulo - " + modulo);
+    }
+
+    public static int getInt() {
+        return new Scanner(System.in).nextInt();
+    }
 }
