a45a5b7d9ceeefdbd06e61e20705a5358c2740f9
Tomasz <feat24@wp.pl>
2023-12-11 09:55:35 +0100

    Add solution Task12

--- src/main/java/task12/BasicMathematicalOperations.java
+++ src/main/java/task12/BasicMathematicalOperations.java
@@ -1,4 +1,24 @@
 package task12;
 
 public class BasicMathematicalOperations {
+
+    protected int addition(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    protected int subtraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    protected int multiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    protected int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    protected int modulo(int firstNumber, int secondNumber) {
+        return firstNumber % secondNumber;
+    }
 }
